25ba6cb1afc47da823dbc9287c643a4c9a0797f4
malshoff <malshoff5@gmail.com>
2019-09-01 20:08:57 -0400

    refactor(core): Use mongodb to store out of queue

--- cron.py
+++ cron.py
@@ -6,6 +6,8 @@ from roster import Roster
 s = SlackBot()
 rost = Roster("password.json", "EAST")
 
+rost.setOutOfQueue()
+
 for engineer in s.inTraining:
     s.setStatus(engineer)
 
--- roster.py
+++ roster.py
@@ -6,7 +6,11 @@ from slacker import Slacker
 from pymongo import MongoClient
 import os
 
-
+CONNECT_STRING = os.environ["CONNECT_STRING"]
+client = MongoClient(f'{CONNECT_STRING}')
+db = client.queue
+out = db.ooq
+timezones = db.timezones
 class Roster(object):
 
     def __init__(self, passwordFile, tz):
@@ -15,26 +19,23 @@ class Roster(object):
         self.UNAVAILABLE = {9, 10, 12, 13, 14, 15, 16}
         self.TODAYS_DATE = datetime.date.today()
         self.TRAINING_CODES = {11}
-
+        self.tz = tz
         with open(str(passwordFile), 'r') as creds:
             self.credentials = json.loads(creds.read())
-        self.updateEmployees(tz)
+        self.updateEmployees()
 
-    def getTimezones(self, tz):
-        CONNECT_STRING = os.environ["CONNECT_STRING"]
-        client = MongoClient(f'{CONNECT_STRING}')
-        db = client.queue
-        timezones = db.timezones
-        print("Finding tz" + str(tz))
-        ea = timezones.find_one({tz: {'$exists': True}})
-        return set(ea[tz])
+    def getTimezones(self):
+        
+        print("Finding tz" + str(self.tz))
+        ea = timezones.find_one({self.tz: {'$exists': True}})
+        return set(ea[self.tz])
 
-    def updateEmployees(self, tz):
+    def updateEmployees(self):
         r = requests.get('https://pivotal-roster-api.cfapps.io/api/employees/employee/', auth=(
             self.credentials['user'],
             self.credentials['pass'])
         )
-        tzs = self.getTimezones(tz)
+        tzs = self.getTimezones()
         for person in r.json():
             if person['timezone'] in tzs:
                 self.ENGINEER_IDS.add(person['id'])
@@ -44,16 +45,28 @@ class Roster(object):
                     'email': person['email']
                 }
 
-    def getOutOfQueue(self):
+    def setOutOfQueue(self):
+
         r = requests.get('https://pivotal-roster-api.cfapps.io/api/schedule/employee_schedule?active=true&audit_date=' +
                          str(self.TODAYS_DATE), auth=(self.credentials['user'], self.credentials['pass']))
         engs = r.json()
         res = []
+
         for eng in engs:
             # engineer is out of queue
             if eng['engineer'] in self.ENGINEER_IDS and eng['availability'] in self.TRAINING_CODES:
                 res.append(self.EMPLOYEES[eng['engineer']])
-        return res
+
+        out.update(
+            {'date': str(self.TODAYS_DATE)},
+            {'$set': res},
+            upsert=True
+        )
+
+    def getOutOfQueue(self):
+        outToday = out.find_one({str(self.TODAYS_DATE):{'$exists': True}})
+        return outToday[str(self.TODAYS_DATE)]
+       
 
 
 '''print(slack.usergroups.users.list('SCY2D900P'))'''  # ID of usergroup
