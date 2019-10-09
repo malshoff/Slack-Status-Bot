a6cf6019320d411f820e57bd5f399f4126e85542
Malachi Shoffner <malshoff5@gmail.com>
2019-10-09 14:40:53 -0400

    feat(tasks): refresh out of queue every minute

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 from celery import Celery
+from celery.schedules import crontab
 import os
 import json
 from slackbot import SlackBot
 from pymongo import MongoClient
 from roster import Roster
+
 
 vcap_services = os.getenv('VCAP_SERVICES', None)
 vcap_services = json.loads(vcap_services)
@@ -18,7 +20,7 @@ redis_url = 'redis://:{0}@{1}:{2}/0'.format(
 BROKER_URL = redis_url
 
 app = Celery('tasks', broker=BROKER_URL)
-
+app.conf.timezone = 'US/Eastern'
 
 CONNECT_STRING = os.environ["CONNECT_STRING"]
 client = MongoClient(f'{CONNECT_STRING}')
@@ -30,6 +32,34 @@ ZOOM_MONGO = os.environ["ZOOM_MONGO"]
 zoomDBClient = MongoClient(ZOOM_MONGO)
 zdb = zoomDBClient.zoom
 zoomUsers = zdb.users
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls refresh every minute.
+    sender.add_periodic_task(60.0, refresh.s(), name='Refresh every minute')
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=8, minute=30),
+        daily.s(),
+    )
+
+@app.task
+def daily():
+    rost = Roster("password.json", "EAST")
+    rost.setEmployees()
+    rost.setOutOfQueue()
+
+    s = SlackBot()
+    print("From cron.py")
+    print(s.inTraining)
+
+    if s.inTraining:
+        for engineer in s.inTraining:
+            s.setStatus(engineer)
+
+    s.msgOutOfQueue()
+    s.msgAllStaff()
 
 @app.task
 def listInMeeting():
@@ -114,7 +144,7 @@ def processEvent(e):
     else:
         print("This message does not apply!")
 
-
+@app.task
 def refresh():
     rost = Roster("password.json", "EAST")
     rost.setEmployees()
